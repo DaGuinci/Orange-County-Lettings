7c2fa054133a0cb24ec02e2678beb971536b555f
DaGuinci <daguinci@mailo.com>
2023-12-26 16:56:29 +0100

    chore(refactor): setup lettings app

--- oc_lettings_site/apps.py
+++ oc_lettings_site/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class OCLettingsSiteConfig(AppConfig):
     name = 'oc_lettings_site'
+
+
+class LettingsAppConfig(AppConfig):
+    """Serve the letting app's name to settings.py"""
+    name = 'lettings'
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'oc_lettings_site.apps.OCLettingsSiteConfig',
+    'oc_lettings_site.apps.LettingsAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
