9e3065c39a2c9cdeb247ea9010350b9bc8458d0d
DaGuinci <daguinci@mailo.com>
2023-12-28 19:12:27 +0100

    feat(errors): custom 404 and 500 pages

--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+def custom_404(request, exception):
+    return render(request, 'home/404.html')
+
+def custom_500(request):
+    return render(request, 'home/500.html')
+
+def error_generating(request):
+    raise Exception("This is a test error")
