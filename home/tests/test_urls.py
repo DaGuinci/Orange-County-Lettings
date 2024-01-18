2ba8790858c6519d6bc1489909275608ded99d45
DaGuinci <daguinci@mailo.com>
2024-01-18 12:13:02 +0100

    doc(tests): docstrings in tests files

--- home/tests/test_urls.py
+++ home/tests/test_urls.py
@@ -3,8 +3,14 @@ from django.urls import reverse
 
 
 class TestUrls(TestCase):
+    """
+    Class testcase pour les urls
+    """
     client = Client()
 
     def test_home_url_is_resolved(self):
+        """
+        Test du fonctionnement du namespace home et de la fonction index
+        """
         url = reverse('home:index')
         assert url == '/'
