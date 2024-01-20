aa8f1310de0e549b9484e43b40803d240f7601a1
DaGuinci <daguinci@mailo.com>
2024-01-20 01:05:23 +0100

    config(settings.py): test Django debug option in env

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -18,10 +18,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+if os.getenv('DEBUG_VALUE') == 'False':
+    DEBUG = False
+else:
+    DEBUG = True
 
 # uncomment to view custom error pages
-ALLOWED_HOSTS = ["*"]
+if DEBUG is True:
+    ALLOWED_HOSTS = []
+elif DEBUG is False:
+    ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
